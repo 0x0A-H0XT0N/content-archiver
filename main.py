97b58a5fa5aca9c35070f8fc6b0ba35f3523f813
PhoenixK7PB <phoenixk7pb@protonmail.ch>
2019-03-24 23:05:36 -0300

    Updated lots of stuff for the main.py

--- main.py
+++ main.py
@@ -10,34 +10,100 @@
 # TODO: make one more advanced progress bar
 # TODO: add choose option for format when downloading
 # TODO: add menu and options
+# TODO: handle exceptions from pytube
+# TODO: make callback functions for completed downloads,
+#  displaying remaining videos (if channel or playlist), time elapsed
+#  and making a file handler function for moving and etc
+# TODO: make a list of downloaded video, when updating use data list for
+#  not repeating downloads
+# TODO: printing filesize (MB) of video before downloading
+# TODO: make a max of video length for downloading using yt.lenght
+# TODO: make a clear func for returning or after finishing a download
+# TODO: make a "Config" file for storing path and downloaded channels
+#  (last update, total videos, etc)
+# TODO: use colorama for font color
+# TODO: pass everything to yt-dl, currently pytube
+#  does not support age restricted videos.
 
 from pytube import YouTube, Playlist
-from pathlib import Path
 from progress.bar import IncrementalBar
+from pathlib import Path
+from time import sleep
 import os
 
+affirmative_choice = ["y", "yes", "s", "sim", "yeah", "yah", "ya"]
+negative_choice = ["n", "no", "nao", "na", "nop", "nah"]
 
-def make_bar(filesize, title):
+
+def clear():
+    """
+    check if the machine is windows or linux,
+    then clear the screen
+    :return: a clean screen :)
+    """
+    if os.name == "nt":
+        os.system('cls')
+
+    else:
+        os.system('clear')
+
+
+def show_menu():
+    """
+    :return: menu banner with options
+    """
+    print(" ███╗   ███╗ ██████╗████████╗ ██████╗ ██╗    ██╗")
+    print(" ████╗ ████║██╔════╝╚══██╔══╝██╔═══██╗██║    ██║")
+    print(" ██╔████╔██║██║  ███╗  ██║   ██║   ██║██║ █╗ ██║")
+    print(" ██║╚██╔╝██║██║   ██║  ██║   ██║   ██║██║███╗██║")
+    print(" ██║ ╚═╝ ██║╚██████╔╝  ██║   ╚██████╔╝╚███╔███╔╝")
+    print(" ╚═╝     ╚═╝ ╚═════╝   ╚═╝    ╚═════╝  ╚══╝╚══╝  ")
+    print("")
+    print("   █████╗ ██████╗  ██████╗██╗  ██╗██╗██╗   ██╗███████╗")
+    print("   ██╔══██╗██╔══██╗██╔════╝██║  ██║██║██║   ██║██╔════╝")
+    print("   ███████║██████╔╝██║     ███████║██║██║   ██║█████╗")
+    print("   ██╔══██║██╔══██╗██║     ██╔══██║██║╚██╗ ██╔╝██╔══╝")
+    print("   ██║  ██║██║  ██║╚██████╗██║  ██║██║ ╚████╔╝ ███████╗")
+    print("   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═══╝  ╚══════╝")
+    print("")
+    print("1) Download a Channel/playlist")
+    print("2) Download a Single video")
+    print("3) Check for new videos")
+    print("4) Set download path")
+    print("5) See downloaded channels")
+    print("6) Make a torrent")
+    print("Press ENTER to exit")
+
+
+def make_bar(youtube_obj, filesize, title):
     """
     make a bar object, need filesize for bar progress,
     :param filesize: filesize of video
     :return: nothing.
     """
     global bar
+    global yt
+    yt = youtube_obj
     bar = IncrementalBar("[download] " + title, max=filesize, suffix='%(percent).2f%%')
 
 
 def show_progress_bar(stream, chunk, file_handle, bytes_remaining):
     try:
         global last_bytes
+        pass
     except NameError:
         pass
+    exit()
     bytes_finished = yt.streams.first().filesize - bytes_remaining
     try:
         bar.next(last_bytes - bytes_remaining)
     except NameError:
         bar.next(bytes_finished)
     last_bytes = bytes_remaining
+    if bytes_remaining == 0:
+        del last_bytes, bytes_remaining, bytes_finished
+        bar.finish()
+        print()
 
 
 def download_video(youtube_obj, path=str(Path.home())):
@@ -51,16 +117,105 @@ def download_video(youtube_obj, path=str(Path.home())):
     """
     if not os.path.exists(path):
         os.makedirs(path)
-    make_bar(youtube_obj.streams.first().filesize, youtube_obj.title)
+    make_bar(youtube_obj, youtube_obj.streams.first().filesize, youtube_obj.title)
     youtube_obj.register_on_progress_callback(show_progress_bar)
     youtube_obj.streams.first().download(path)
 
 
-yt = YouTube(str(input("Video url to download: ")))
-path_location = str(input("Full path to save file (blank is home directory): "))
+def playlist_download(url_list):
+    for url_pl in url_list:
+        yt_pl = YouTube(url_pl)
+        download_video(yt_pl)
+    input("Finished... Press any key to go back.")
+    clear()
 
-if path_location != "":
-    download_video(yt, path_location)
-else:
-    download_video(yt)
-print()
+# yt = YouTube(str(input("Video url to download: ")))
+# path_location = str(input("Full path to save file (blank is home directory): "))
+#
+# if path_location != "":
+#     download_video(yt, path_location)
+# else:
+#     download_video(yt)
+# print()
+
+
+if __name__ == "__main__":
+    maintainer = True
+
+    while maintainer:
+        show_menu()  # show menu
+        choice = input("\n>: ")  # wait for user input
+
+        if choice == "":
+            clear()
+            exit()
+
+        try:
+            choice = int(choice)  # try to convert choice(str) to choice(int),
+            # this is needed because the normal input is a str
+
+        except ValueError:  # if the int() parser cant convert, raises a ValueError, this take care if it
+            clear()  # clear the screen
+            print("Numbers only.")
+            input("Press any key to go back...\n")  # wait for user input
+            clear()
+            continue  # goes right back in the loop, skip the "else:" later on, save time
+
+        if choice == 1:
+            clear()
+            print("Download channel/playlist selected... Press enter to return.\n")
+            video_url = str(input("Video url to download: "))  # wait for user input,
+
+            if video_url == "":  # validate user input,if it's empty, go to the menu
+                clear()
+                print("Going back...")
+                sleep(1)
+                clear()
+                continue
+
+            pl = Playlist(video_url)  # after validation make a Playlist() obj
+            sleep(0.5)
+            clear()
+            print("Searching for videos...")
+            try:
+                parsed_links = pl.parse_links()
+            except ValueError:
+                clear()
+                print("Unknown URL type... Going back.")
+                sleep(2)
+                clear()
+                continue
+            clear()
+            choice = input("Founded %d videos... Continue? [Y/n]\n>:" % (len(parsed_links)))
+            if choice in negative_choice:
+                clear()
+                print("Going back...")
+                sleep(1)
+                clear()
+                continue
+            else:
+                clear()
+                playlist_download(parsed_links)
+
+
+        elif choice == 2:
+            print
+            "Menu 2 has been selected"
+            ## You can add your code or functions here
+        elif choice == 3:
+            print
+            "Menu 3 has been selected"
+            ## You can add your code or functions here
+        elif choice == 4:
+            print
+            "Menu 4 has been selected"
+            ## You can add your code or functions here
+        elif choice == 5:
+            print
+            "Menu 5 has been selected"
+            ## You can add your code or functions here
+            loop = False  # This will make the while loop to end as not value of loop is set to False
+        else:
+            clear()
+            input("No option located. Press any key to go back...")
+            clear()
